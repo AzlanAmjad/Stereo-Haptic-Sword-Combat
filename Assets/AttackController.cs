48efa1f8794881e41be72fee6430ba1b0631bf10
Tr1d3nt <saudagha2@gmail.com>
2023-11-30 02:00:48 -0700

    adding enemy collision detection

---
+++ Assets/AttackController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackController : MonoBehaviour
+{
+    public EnemyScript enemyScript;
+    public FloatingHealthBar floatingHealthBar; 
+    public float damage = 0.5f;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Touchable") && other.GetComponent<EnemyScript>())
+        {
+            enemyScript = other.GetComponent<EnemyScript>();
+            enemyScript.TakeDamage(damage); 
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
